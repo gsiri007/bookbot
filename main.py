43377a5fefd4e2dc7b6fed0c95d614f374cc1fee
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2025-06-20 06:29:16 +0530

    add command line argument option

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from stats import get_book_text, get_num_words, get_character_count, get_ch_count_list
+import sys
 
 def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1] 
     print("============ BOOKBOT ============")
-    book_path = "./books/frankenstein.txt"
 
     content = get_book_text(book_path)
     print(f"Analyzing book found at {book_path}")
